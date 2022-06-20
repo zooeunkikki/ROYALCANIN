4b7dad4428a0f3fae8c3dc6ac874121bbc649aa8
zooeunkikki <rlawndms517@gmail.com>
2022-06-20 15:23:46 +0900

    mamamama

---
+++ css/slide/right_slide.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function () {
+    $(".right-slide").slick({
+        dots: true,
+        // vertical: true,
+        // centerMode: true,
+        slidesToShow: 1,    
+        //몇장씩 보여줄껀지
+        slidesToScroll: 1
+        //몇개의 배너를 이동할껀지
+    });
+});
