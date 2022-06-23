84306c0741a9da94755549a95b771c9f3e9593e7
zooeunkikki <rlawndms517@gmail.com>
2022-06-23 16:34:38 +0900

    js

---
+++ js/index.js
@@ -0,0 +1,88 @@
+//pink-btn-slide
+
+const PinkBox = document.querySelector('.right-review');
+const RiviBox = document.querySelectorAll('.right-review .ri-vi-box li');
+const pink_slide = document.querySelector('.silde-box label>span');
+let CheckSlide2 = document.querySelector('.check-slide2');
+CheckSlide2.classList.add('active');
+let H4;
+let H3;
+let hLine;
+
+function h4_change(){
+    let H4 = document.querySelector('.slide-h4');
+    let H3 = document.querySelectorAll('.silde-box>hgroup>h3');
+    let hLine = document.querySelector('.right-review label');
+
+    H4.innerHTML = `집사님과 댕냥이들의
+    100% 리얼한 후기들을
+    만나보세요!
+    `;
+    H4.style = "color:#06337C";
+    PinkBox.classList.add('bgcolor');
+    pink_slide.style  = "background-color:#699CFF; transition: 0.5s ease;";
+    hLine.style = "border:1px solid #699CFF; ";
+    H3.forEach(function(aa,bb){
+        H3[0].style = "color:#06337C";
+        H3[1].style = "color:#FFF";
+    });
+};
+
+function h4_before(){
+    let H4 = document.querySelector('.slide-h4');
+    let H3 = document.querySelectorAll('.silde-box>hgroup>h3');
+    let hLine = document.querySelector('.silde-box label');
+    H4.innerHTML = `집사님들의 많은 리뷰들과
+                            구매하신 제품들부터
+                            댕냥이님들이 만족한 최애템까지 보려면?`;
+    H4.style = "color:#5F0D0D";
+    PinkBox.classList.remove('bgcolor');
+    pink_slide.style  = "background-color:#FF9290; transition: 0.5s ease;";
+    hLine.style = "border:1px solid #FF9290;";
+    H3.forEach(function(aa,bb){
+        H3[0].style = "color:#5F0D0D";
+        H3[1].style = "color:#987272";
+    });
+}
+
+
+let slideBtn;
+
+//버튼이 먼저 움직이는거 방지 -> 클릭문 안에 넣기!!
+pink_slide.addEventListener('click', function () {
+        slideBtn = setTimeout(function () {
+        clearTimeout(slideBtn);
+        //slide-box txt change
+        let RiSlide2 = document.querySelector('.ri-slide2');
+        if (!RiSlide2.classList.contains('active')) {
+            CheckSlide2.classList.remove('active');
+            RiSlide2.classList.add('active');
+
+            // bg & txt
+            h4_change();
+
+        } else {
+            RiSlide2.classList.remove('active');
+            CheckSlide2.classList.add('active');
+
+            //bg & txt
+            h4_before();   
+        }
+        //console.log(!RiSlide2.classList.contains('active'))
+    },50)
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/royalcanin_sub_8_service_center.js
+++ js/royalcanin_sub_8_service_center.js
@@ -1,18 +1,44 @@
 //팝업기능
-const Pop_up = document.querySelectorAll('.rank-10');
-let pop_box = document.querySelectorAll('.rank-pop');
+const Pop_up = document.querySelectorAll('.rank-box');
+const Pop_box = document.querySelectorAll('.rank-pop li'); 
 
 Pop_up.forEach(function(p,k){
     Pop_up[k].addEventListener('click',function(){
         event.preventDefault();
-        let pop = document.querySelector('.pop');
-        pop.classList.add('active');
-        let x_box = document.querySelector('.rank-pop li div a');
-        x_box.addEventListener('click',function(){
-            pop.classList.remove('active');
+
+            let pop = document.querySelector('.pop');
+            pop.classList.add('active')
+            Pop_box[k].classList.add('active');
+
+
+
+        const x_box = document.querySelectorAll('.rank-pop li div a');
+        x_box.forEach(function(xb,key){
+            x_box[key].addEventListener('click',function(){
+                pop.classList.remove('active');
+                Pop_box[k].classList.remove('active');
+            });
         });
+        
+
     });
 });
+
+
+//Fna-btn
+const  fna_btn = document.querySelectorAll('.fna-select-box ul li');
+const suject =document.querySelectorAll('.fna-subject');
+let oxo =0;
+fna_btn.forEach(function(btn,b){
+    console.log(fna_btn[b])
+    fna_btn[b].addEventListener('click',function(){
+        event.preventDefault();
+        suject[oxo].classList.remove('active');
+        suject[b].classList.add('active');
+        oxo=b;
+    });
+});
+
 
 
 
